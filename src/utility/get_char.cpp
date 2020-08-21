c4b258d0a12353ce919a967e02033dcc99560505
Cam Mannett <cmannett85@users.noreply.github.com>
2020-08-21 07:08:28 +0100

    #83 Fix "Blocking on the main thread is very dangerous" warning (#86)
    Fix #83

---
+++ src/utility/get_char.cpp
@@ -0,0 +1,98 @@
+/* Cam Mannett 2020
+ *
+ * See LICENSE file
+ */
+
+#include "malbolge/utility/get_char.hpp"
+
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
+
+#include <map>
+#include <array>
+#endif
+
+using namespace malbolge;
+
+#ifdef EMSCRIPTEN
+namespace
+{
+struct em_istream {
+    em_istream() :
+        pos{0},
+        len{0}
+    {}
+
+    std::size_t pos;
+    std::size_t len;
+    std::array<char, 4096> buffer;
+};
+
+std::map<std::istream*, em_istream> streams;
+}
+
+// From https://github.com/curiousdannii/emglken/blob/master/src/getc.c
+EM_JS(int, malbolge_input, (char* buffer, int maxlen), {
+    return Asyncify.handleAsync(async () => {
+        // Wrap the worker's onmessage function so we can catch our messages.
+        // This IS a hack, but Emscripten doesn't allow us to add our own
+        // custom message handler for pthread emulation
+        if (typeof Module.malbolgeConfigured == "undefined") {
+            Module.malbolgeConfigured = true;
+            Module.malbolgeInputQueue = [];
+            Module.malbolgeInputNotify = function() {};
+
+            let standardHandlers = self.onmessage;
+            self.onmessage = function(e) {
+                if (e.data.cmd === "input_text") {
+                    const encoder = new TextEncoder();
+                    const u8Text = encoder.encode(e.data.input_text);
+                    Module.malbolgeInputQueue.push(u8Text);
+                    Module.malbolgeInputNotify();
+                } else {
+                    standardHandlers(e);
+                }
+            };
+        }
+
+        // If there is no input waiting to be processed, block until notified
+        // that there is
+        if (!Module.malbolgeInputQueue.length) {
+            await new Promise(resolve => {
+                Module.malbolgeInputNotify = resolve;
+            });
+        }
+
+        // Extract the first buffer's contents (limited to the max length), and
+        // pass to the input buffer
+        const input = Module.malbolgeInputQueue[0];
+        const len = Math.min(input.length, maxlen);
+        HEAPU8.set(input.subarray(0, len), buffer);
+
+        if (len == input.length) {
+            Module.malbolgeInputQueue.shift();
+        } else {
+            Module.malbolgeInputQueue[0] = input.subarray(len);
+        }
+
+        return len;
+    });
+});
+#endif
+
+std::optional<char> utility::get_char(std::istream& str)
+{
+#ifdef EMSCRIPTEN
+    auto& [pos, len, buffer] = streams[&str];
+
+    if (pos == len) {
+        len = malbolge_input(buffer.data(), buffer.size());
+        pos = 0;
+    }
+
+    return buffer[pos++];
+#else
+    auto c = char{};
+    return str.get(c) ? std::optional<char>{c} : std::optional<char>{};
+#endif
+}
