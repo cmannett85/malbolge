a18e95dfeb0ca01d4f702dc6022217812ec90c6f
Cam Mannett <camden.mannett@protonmail.ch>
2020-08-21 07:21:06 +0100

    #79 Remove '_wasm' suffix from WASM build output
    Fix #79

--- cmake/build_types/wasm.cmake
+++ cmake/build_types/wasm.cmake
@@ -13,6 +13,10 @@ add_dependencies(malbolge_wasm gen_version)
 target_compile_features(malbolge_wasm PUBLIC cxx_std_20)
 set_target_properties(malbolge_wasm PROPERTIES CXX_EXTENSIONS OFF)
 
+# The output files have .wasm and .js extensions, so there's no need for the
+# _wasm suffix
+set_target_properties(malbolge_wasm PROPERTIES OUTPUT_NAME malbolge)
+
 # This is set by the Emscripten environment, but adding here helps the IDE
 target_compile_definitions(malbolge_wasm
     PUBLIC EMSCRIPTEN
