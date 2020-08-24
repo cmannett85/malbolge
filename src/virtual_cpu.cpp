a9818a10ba18a341e82f55c408396ca7834b00e7
Cam Mannett <cmannett85@users.noreply.github.com>
2020-08-24 11:00:52 +0100

    #92 Create C interface for library (#98)
    Fix #92

--- src/virtual_cpu.cpp
+++ src/virtual_cpu.cpp
@@ -13,6 +13,108 @@
 using namespace malbolge;
 using namespace std::string_literals;
 
+namespace
+{
+void vcpu_loop(virtual_memory& vmem,
+               std::atomic<virtual_cpu::execution_state>& state,
+               std::function<void ()> waiting_for_input,
+               std::istream& istr,
+               std::ostream& ostr)
+{
+    auto a = math::ternary{};   // Accumulator register
+    auto c = vmem.begin();      // Code pointer
+    auto d = vmem.begin();      // Data pointer
+
+    // Loop forever, but increment the pointers on each iteration
+    auto step = std::size_t{0};
+    for (; true; ++c, ++d, ++step) {
+        if (state == virtual_cpu::execution_state::STOPPED) {
+            return;
+        }
+
+        // Pre-cipher the instruction
+        auto instr = pre_cipher_instruction(*c, c - vmem.begin());
+        if (!instr) {
+            throw execution_exception{
+                "Pre-cipher non-whitespace character must be graphical "
+                    "ASCII: " + std::to_string(static_cast<int>(*c)),
+                step
+            };
+        }
+
+        log::print(log::VERBOSE_DEBUG,
+                   "Step: ", step, ", pre-cipher instr: ",
+                   static_cast<int>(*instr));
+
+        switch (*instr) {
+        case cpu_instruction::set_data_ptr:
+            d = vmem.begin() + static_cast<std::size_t>(*d);
+            break;
+        case cpu_instruction::set_code_ptr:
+            c = vmem.begin() + static_cast<std::size_t>(*d);
+            break;
+        case cpu_instruction::rotate:
+            a = d->rotate();
+            break;
+        case cpu_instruction::op:
+            a = *d = a.op(*d);
+            break;
+        case cpu_instruction::read:
+        {
+            if (waiting_for_input) {
+                waiting_for_input();
+            }
+
+            auto input = utility::get_char(istr);
+            if (input) {
+                a = *input;
+            } else {
+                a = math::ternary::max;
+            }
+
+            break;
+        }
+        case cpu_instruction::write:
+            if (a != math::ternary::max) {
+#ifdef EMSCRIPTEN
+                // Emscripten cannot flush output without a newline, so
+                // we need to force with std::endl
+                if (static_cast<char>(a) == '\n') {
+                    ostr << std::endl;
+                } else {
+                    ostr << static_cast<char>(a);
+                }
+#else
+                ostr << static_cast<char>(a);
+#endif
+            }
+            break;
+        case cpu_instruction::stop:
+            return;
+        default:
+            // Nop
+            break;
+        }
+
+        // Post-cipher the instruction
+        auto pc = post_cipher_instruction(*c);
+        if (!pc) {
+            throw execution_exception{
+                "Post-cipher non-whitespace character must be graphical "
+                    "ASCII: " + std::to_string(static_cast<int>(*c)),
+                step
+            };
+        }
+        *c = *pc;
+
+        log::print(log::VERBOSE_DEBUG,
+                   "\tPost-op regs - a: ", a,
+                   ", c[", std::distance(vmem.begin(), c), "]: ", *c,
+                   ", d[", std::distance(vmem.begin(), d), "]: ", *d);
+    }
+}
+}
+
 virtual_cpu::~virtual_cpu()
 {
     *state_ = execution_state::STOPPED;
@@ -44,7 +146,7 @@ std::future<void> virtual_cpu::run(std::istream& istr, std::ostream& ostr)
     }
     *state_ = execution_state::RUNNING;
 
-    log::print(log::DEBUG, "Starting program");
+    log::print(log::DEBUG, "Starting program (future-based)");
 
     auto promise = std::promise<void>{};
     auto fut = promise.get_future();
@@ -55,10 +157,6 @@ std::future<void> virtual_cpu::run(std::istream& istr, std::ostream& ostr)
                            &istr,
                            &ostr]() mutable {
         log::print(log::VERBOSE_DEBUG, "Program thread started");
-
-        auto a = math::ternary{};   // Accumulator register
-        auto c = vmem.begin();      // Code pointer
-        auto d = vmem.begin();      // Data pointer
 
         auto exception = false;
         auto stopped_setter = utility::raii{[&]() {
@@ -73,99 +171,57 @@ std::future<void> virtual_cpu::run(std::istream& istr, std::ostream& ostr)
             log::print(log::DEBUG, "Program thread exiting");
         }};
 
-        // Loop forever, but increment the pointers on each iteration
-        auto step = std::size_t{0};
-        for (; true; ++c, ++d, ++step) {
-            if (*state == execution_state::STOPPED) {
-                return;
-            }
-
-            try {
-                // Pre-cipher the instruction
-                auto instr = pre_cipher_instruction(*c, c - vmem.begin());
-                if (!instr) {
-                    throw execution_exception{
-                        "Pre-cipher non-whitespace character must be graphical "
-                            "ASCII: " + std::to_string(static_cast<int>(*c)),
-                        step
-                    };
-                }
-
-                log::print(log::VERBOSE_DEBUG,
-                           "Step: ", step, ", pre-cipher instr: ",
-                           static_cast<int>(*instr));
-
-                switch (*instr) {
-                case cpu_instruction::set_data_ptr:
-                    d = vmem.begin() + static_cast<std::size_t>(*d);
-                    break;
-                case cpu_instruction::set_code_ptr:
-                    c = vmem.begin() + static_cast<std::size_t>(*d);
-                    break;
-                case cpu_instruction::rotate:
-                    a = d->rotate();
-                    break;
-                case cpu_instruction::op:
-                    a = *d = a.op(*d);
-                    break;
-                case cpu_instruction::read:
-                {
-                    auto input = utility::get_char(istr);
-                    if (input) {
-                        a = *input;
-                    } else {
-                        a = math::ternary::max;
-                    }
-
-                    break;
-                }
-                case cpu_instruction::write:
-                    if (a != math::ternary::max) {
-#ifdef EMSCRIPTEN
-                        // Emscripten cannot flush output without a newline, so
-                        // we need to force with std::endl
-                        if (static_cast<char>(a) == '\n') {
-                            ostr << std::endl;
-                        } else {
-                            ostr << static_cast<char>(a);
-                        }
-#else
-                        ostr << static_cast<char>(a);
-#endif
-                    }
-                    break;
-                case cpu_instruction::stop:
-                    return;
-                default:
-                    // Nop
-                    break;
-                }
-
-                // Post-cipher the instruction
-                auto pc = post_cipher_instruction(*c);
-                if (!pc) {
-                    throw execution_exception{
-                        "Post-cipher non-whitespace character must be graphical "
-                            "ASCII: " + std::to_string(static_cast<int>(*c)),
-                        step
-                    };
-                }
-                *c = *pc;
-
-                log::print(log::VERBOSE_DEBUG,
-                           "\tPost-op regs - a: ", a,
-                           ", c[", std::distance(vmem.begin(), c), "]: ", *c,
-                           ", d[", std::distance(vmem.begin(), d), "]: ", *d);
-            } catch (std::exception& e) {
-                auto e_ptr = std::make_exception_ptr(std::move(e));
-                p.set_exception(std::move(e_ptr));
-                exception = true;
-                return;
-            }
+        try {
+            vcpu_loop(vmem, *state, {}, istr, ostr);
+        } catch (std::exception& e) {
+            auto e_ptr = std::make_exception_ptr(std::move(e));
+            p.set_exception(std::move(e_ptr));
+            exception = true;
+            return;
         }
     }};
 
     return fut;
+}
+
+void virtual_cpu::run(std::function<void (std::exception_ptr)> stopped,
+                      std::function<void ()> waiting_for_input,
+                      std::istream& istr,
+                      std::ostream& ostr)
+{
+    if (*state_ != execution_state::READY) {
+        throw execution_exception{"vCPU is not in a ready state", 0};
+    }
+    *state_ = execution_state::RUNNING;
+
+    log::print(log::DEBUG, "Starting program (callback-based)");
+
+    thread_ = std::thread{[vmem = std::move(vmem_),
+                           state = state_,
+                           stopped_cb = std::move(stopped),
+                           waiting_cb = std::move(waiting_for_input),
+                           &istr,
+                           &ostr]() mutable {
+        log::print(log::VERBOSE_DEBUG, "Program thread started");
+
+        auto exception = std::exception_ptr{};
+        auto stopped_setter = utility::raii{[&]() {
+            *state = execution_state::STOPPED;
+            stopped_cb(exception);
+
+#ifdef EMSCRIPTEN
+            ostr << std::endl;
+#endif
+            log::print(log::DEBUG, "Program thread exiting");
+        }};
+
+        try {
+            vcpu_loop(vmem, *state, waiting_cb, istr, ostr);
+        } catch (std::exception& e) {
+            exception = std::current_exception();
+            return;
+        }
+    }};
 }
 
 void virtual_cpu::stop()
---
+++ test/c_interface_test.cpp
@@ -0,0 +1,226 @@
+/* Cam Mannett 2020
+ *
+ * See LICENSE file
+ */
+
+#include "malbolge/c_interface.hpp"
+#include "malbolge/version.hpp"
+#include "malbolge/exception.hpp"
+#include "malbolge/virtual_memory.hpp"
+
+#include "test_helpers.hpp"
+
+#include <filesystem>
+#include <iterator>
+#include <fstream>
+#include <chrono>
+#include <thread>
+
+using namespace malbolge;
+using namespace std::chrono_literals;
+
+namespace
+{
+std::vector<char> load_program_from_disk(const std::filesystem::path& path)
+{
+    const auto file_size = std::filesystem::file_size(path);
+    auto data = std::vector<char>{};
+    data.reserve(file_size);
+
+    {
+        auto stream = std::ifstream{};
+        stream.exceptions(std::ios::badbit | std::ios::failbit);
+        stream.open(path, std::ios::binary);
+
+        std::copy(std::istreambuf_iterator<char>{stream},
+                  std::istreambuf_iterator<char>{},
+                  std::back_inserter(data));
+    }
+
+    return data;
+}
+
+malbolge_virtual_cpu global_vcpu = nullptr;
+auto global_stopped = false;
+void stopped_cb(int err, malbolge_virtual_cpu handle)
+{
+    BOOST_CHECK_EQUAL(err, 0);
+    BOOST_CHECK_EQUAL(handle, global_vcpu);
+    global_stopped = true;
+}
+
+auto global_waiting = false;
+void waiting_cb(malbolge_virtual_cpu handle)
+{
+    BOOST_CHECK_EQUAL(handle, global_vcpu);
+    global_waiting = true;
+}
+}
+
+BOOST_AUTO_TEST_SUITE(c_interface_suite)
+
+BOOST_AUTO_TEST_CASE(log_level)
+{
+    auto f = [](auto level, auto success) {
+        const auto r = malbolge_set_log_level(level);
+        BOOST_CHECK_EQUAL(r, success);
+
+        if (!success) {
+            const auto lvl = malbolge_log_level();
+            BOOST_CHECK_EQUAL(lvl, level);
+        }
+    };
+
+    test::data_set(
+        f,
+        {
+            std::tuple{0, 0},
+            std::tuple{1, 0},
+            std::tuple{2, 0},
+            std::tuple{3, 0},
+            std::tuple{4, -EINVAL},
+        }
+    );
+}
+
+BOOST_AUTO_TEST_CASE(version)
+{
+    BOOST_CHECK_EQUAL(std::strcmp(malbolge_version(), version_string), 0);
+}
+
+BOOST_AUTO_TEST_CASE(load)
+{
+    using pdata_t = std::tuple<std::vector<char>,
+                               std::vector<char>,
+                               optional_source_location,
+                               bool>;
+
+    auto f = [](auto program_data, auto parse_data, auto loc, auto fail) {
+        auto fail_line = 0u;
+        auto fail_column = 0u;
+        const auto r = malbolge_load_program(program_data.data(),
+                                             program_data.size(),
+                                             loc ? &fail_line : nullptr,
+                                             loc ? &fail_column : nullptr);
+        BOOST_CHECK_EQUAL(!r, fail);
+
+        if (r) {
+            const auto& vmem = *static_cast<virtual_memory*>(r);
+            for (auto i = 0u; i < parse_data.size(); ++i) {
+                BOOST_CHECK_EQUAL(parse_data[i], vmem[i]);
+            }
+        }
+
+        if (loc) {
+            BOOST_CHECK(fail);
+            BOOST_CHECK_EQUAL(loc->line, fail_line);
+            BOOST_CHECK_EQUAL(loc->column, fail_column);
+        } else {
+            BOOST_CHECK_EQUAL(0, fail_line);
+            BOOST_CHECK_EQUAL(0, fail_column);
+        }
+    };
+
+    test::data_set(
+        f,
+        {
+            pdata_t{{},
+                    {},
+                    {},
+                    true},
+            pdata_t{{40},
+                    {},
+                    {},
+                    true},
+            pdata_t{{40, 39},
+                    {40, 39},
+                    {},
+                    false},
+            pdata_t{{' ', '\0'},
+                    {},
+                    source_location{1, 2},
+                    true},
+            pdata_t{{' ', '\t', '\n'},
+                    {},
+                    {},
+                    true},
+            pdata_t{{' ', '\t', '\n', 40},
+                    {40},
+                    {},
+                    true},
+            pdata_t{{' ', '\t', '\n', 40, 39},
+                    {40, 39},
+                    {},
+                    false},
+        }
+    );
+}
+
+// These next two tests kinda stink because I can't test the cout output
+// without somewhat ruining the simplicity of the C interface (i.e. I can't
+// redirect cout because there are no iostreams in C).  Thankfully the
+// underlying C++ code is fully tested, so it's not too bad
+BOOST_AUTO_TEST_CASE(run_hello_world)
+{
+    malbolge_set_log_level(1);
+
+    auto buffer = load_program_from_disk(std::filesystem::path{"programs/hello_world.mal"});
+    auto fail_line = 0u;
+    auto fail_column = 0u;
+    auto vmem = malbolge_load_program(buffer.data(),
+                                      buffer.size(),
+                                      &fail_line,
+                                      &fail_column);
+    BOOST_REQUIRE(vmem);
+    BOOST_CHECK_EQUAL(fail_line, 0);
+    BOOST_CHECK_EQUAL(fail_column, 0);
+
+    global_stopped = false;
+    global_vcpu = malbolge_vcpu_run(vmem, stopped_cb, waiting_cb, 1);
+    BOOST_REQUIRE(global_vcpu);
+
+    std::this_thread::sleep_for(100ms);
+    BOOST_CHECK(global_stopped);
+
+    const auto r = malbolge_vcpu_input(global_vcpu, "Goodbye!", 8);
+    BOOST_CHECK_NE(r, 0);
+}
+
+BOOST_AUTO_TEST_CASE(run_echo)
+{
+    malbolge_set_log_level(1);
+
+    auto buffer = load_program_from_disk(std::filesystem::path{"programs/echo.mal"});
+    auto fail_line = 0u;
+    auto fail_column = 0u;
+    auto vmem = malbolge_load_program(buffer.data(),
+                                      buffer.size(),
+                                      &fail_line,
+                                      &fail_column);
+    BOOST_REQUIRE(vmem);
+    BOOST_CHECK_EQUAL(fail_line, 0);
+    BOOST_CHECK_EQUAL(fail_column, 0);
+
+    global_stopped = false;
+    global_waiting = true;
+    global_vcpu = malbolge_vcpu_run(vmem, stopped_cb, waiting_cb, 0);
+    BOOST_REQUIRE(global_vcpu);
+
+    std::this_thread::sleep_for(100ms);
+    BOOST_CHECK(global_waiting);
+    global_waiting = false;
+    auto r = malbolge_vcpu_input(global_vcpu, "Hello!", 6);
+    BOOST_CHECK_EQUAL(r, 0);
+
+    std::this_thread::sleep_for(100ms);
+    BOOST_CHECK(global_waiting);
+    global_waiting = false;
+    r = malbolge_vcpu_input(global_vcpu, "Goodbye!", 8);
+    malbolge_vcpu_stop(global_vcpu);
+    BOOST_CHECK_EQUAL(r, 0);
+
+    std::this_thread::sleep_for(100ms);
+    BOOST_CHECK(global_stopped);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
